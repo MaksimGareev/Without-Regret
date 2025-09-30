0df47eeb82da7bbd5f975f10638b5e30710a90cd
VictorAA1116 <156972036+VictorAA1116@users.noreply.github.com>
2025-09-29 21:58:15 -0400

    Set up programming in blockout scene

--- Assets/(will be deleted) Art Scene/First_Person.cs
+++ Assets/(will be deleted) Art Scene/First_Person.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class First_Person : MonoBehaviour
 {
@@ -28,6 +29,11 @@ public class First_Person : MonoBehaviour
     {
         HandleMouseLook();
         HandleMovement();
+
+        if (Input.GetKey(KeyCode.M))
+        {
+            SceneManager.LoadScene("GraveyardLevelBlockout");
+        }
     }
 
     void LateUpdate()
