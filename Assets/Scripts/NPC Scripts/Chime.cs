898ecfbfc5b08c116257403349c15c70728c1914
Tom-Dean <Thomas.dean.iii1002@gmail.com>
2025-11-11 20:43:21 -0500

    Irene Dialogue

--- Assets/Scripts/NPC Scripts/Chime.cs
+++ Assets/Scripts/NPC Scripts/Chime.cs
@@ -20,26 +20,10 @@ public class Chime : MonoBehaviour
     private Transform OrbitPivot;
     private Transform BobObject;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        // orbit pivot at runtime
-       /* OrbitPivot = new GameObject("OrbitPivot").transform;
-        OrbitPivot.position = player.position;
-
-        //BobObject = this.transform;
-        //BobObject.SetParent(OrbitPivot);
-        transform.SetParent(OrbitPivot);
-        BobObject.localPosition = new Vector3(OrbitRadius, 0f, 0f);*/
-    }
-
     // Update is called once per frame
     void LateUpdate()
     {
         if (player == null) return;
-
-        // Keep pivot centered on the player
-        //OrbitPivot.position = player.position;
 
         // Orbit angle increases steadily
         OrbitAngle += OrbitSpeed * Time.deltaTime;
@@ -62,19 +46,5 @@ public class Chime : MonoBehaviour
                 transform.rotation = Quaternion.Slerp(transform.rotation, lookRot, Time.deltaTime * lookSmooth);
             }
         }
-
-        //OrbitPivot.rotation = Quaternion.Euler(0f, OrbitAngle * Mathf.Rad2Deg, 0f);
-
-        // Orbit position (X/Z circle around player)
-        //float x = Mathf.Cos(OrbitAngle) * OrbitRadius;
-        //float z = Mathf.Sin(OrbitAngle) * OrbitRadius;
-
-        // Bobbing motion (sin wave up/down)
-       // float y = Mathf.Sin(Time.time * BobSpeed) * BobHeight;
-       // transform.localPosition = new Vector3(OrbitRadius, y + 1f, 0f);
-
-       // Vector3 lookDir = (player.position - transform.position).normalized;
-       // Quaternion targetRot = Quaternion.LookRotation(lookDir);
-       // transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, Time.deltaTime * 5f);
     }
 }
