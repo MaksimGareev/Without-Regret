03c023fa25d3580fdc0a352dd985b7db9eb9a597
da413 <danielwolfcol@gmail.com>
2025-11-22 16:08:19 -0500

    revised POICustscene
    - also restricts player movement while cutscene is playing

--- Assets/Scripts/Camera Scripts/POICutsceneTrigger.cs
+++ Assets/Scripts/Camera Scripts/POICutsceneTrigger.cs
@@ -1,27 +1,38 @@
 using UnityEngine;
 using System.Collections;
 
+
 public class POICutsceneTrigger : MonoBehaviour
 {
     [SerializeField] private GameObject VCam1;
+    PlayerController playerController;
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Player"))
         {
             VCam1.SetActive(false);
-           // StartCoroutine(ReEnableCam());
+            playerController = other.gameObject.GetComponent<PlayerController>(); 
+            playerController.MovementLocked = true;   
         }
-
         StartCoroutine(ReEnableCam());
         
     }
-
-
     IEnumerator ReEnableCam()
     {
-        yield return new WaitForSeconds(5);
+        yield return new WaitForSeconds(3);
         VCam1.SetActive(true);
+        
+        StartCoroutine(ReEnableMovement());
     }
+
+    IEnumerator ReEnableMovement()
+    {
+        yield return new WaitForSeconds(1);
+        playerController.MovementLocked = false;
+        playerController.enabled = true;
+        gameObject.SetActive(false); 
+    }
+   
 
 }
