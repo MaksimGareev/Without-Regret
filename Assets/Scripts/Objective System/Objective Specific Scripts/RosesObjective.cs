530ed62c6320c865e181eef8a64d04783518deeb
VictorAA1116 <156972036+VictorAA1116@users.noreply.github.com>
2025-11-24 23:21:58 -0500

    fixed echos house objectives

--- Assets/Scripts/Objective System/Objective Specific Scripts/RosesObjective.cs
+++ Assets/Scripts/Objective System/Objective Specific Scripts/RosesObjective.cs
@@ -2,8 +2,8 @@ using UnityEngine;
 
 public class RosesObjective : MonoBehaviour
 {
-    [SerializeField] ObjectiveData linkedObjective;
-    [SerializeField] private string roseItemID = "Roses";
+    [SerializeField] private ObjectiveData linkedObjective;
+    [SerializeField] private ItemData linkedItem;
 
     private void OnEnable()
     {
@@ -18,10 +18,14 @@ public class RosesObjective : MonoBehaviour
     private void CheckForRose(ItemData item)
     {
         // Identify "rose" however your ItemData is structured
-        if (item.name == roseItemID)
+        if (item.ItemType == linkedItem.ItemType && item.ItemName == linkedItem.ItemName)
         {
             ObjectiveManager.Instance.AddProgress(linkedObjective.objectiveID, 1);
             Debug.Log("Rose objective progress increased!");
+        }
+        else
+        {
+            Debug.Log($"{gameObject.name}: Item added is not the linked item. Item Name: {item.ItemName}, Item Type: {item.ItemType}, Expected Name: {linkedItem.ItemName}, Expected Type: {linkedItem.ItemType}");
         }
     }
 }
