9fa8ecfe48d41a9a437a67eb3687cc4035c20145
VictorAA1116 <156972036+VictorAA1116@users.noreply.github.com>
2025-11-24 19:03:08 -0500

    Player position and item positions fixed for saving

--- Assets/Scripts/SaveSystem/SaveData.cs
+++ Assets/Scripts/SaveSystem/SaveData.cs
@@ -3,10 +3,18 @@ public class SaveData
 {
     public string lastSceneName;
     
-    public PlayerSaveData playerSaveData = new PlayerSaveData();
-    public ObjectiveSaveData objectiveSaveData = new ObjectiveSaveData();
-    public WorldSaveData worldSaveData = new WorldSaveData();
-    public InventorySaveData inventorySaveData = new InventorySaveData();
+    public PlayerSaveData playerSaveData;
+    public ObjectiveSaveData objectiveSaveData;
+    public WorldSaveData worldSaveData;
+    public InventorySaveData inventorySaveData;
 
     public int version = 1;
+
+    public SaveData()
+    {
+        playerSaveData = new PlayerSaveData();
+        objectiveSaveData = new ObjectiveSaveData();
+        worldSaveData = new WorldSaveData();
+        inventorySaveData = new InventorySaveData();
+    }
 }
