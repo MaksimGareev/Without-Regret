10483c4c5bce51bbd78091526c5a69ac3eb93a33
VictorAA1116 <156972036+VictorAA1116@users.noreply.github.com>
2025-11-26 22:37:21 -0500

    Some fixes

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -114,7 +114,7 @@ public class PlayerController : MonoBehaviour, ISaveable
 
         if (moveInput != Vector2.zero)
         {
-            Debug.Log("MOVE INPUT: " + moveInput);
+            //Debug.Log("MOVE INPUT: " + moveInput);
         }
     }
 
