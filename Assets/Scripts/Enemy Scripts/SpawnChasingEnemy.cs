eb7abd34756b0b577933bc619eae54290c0c86fc
Tom-Dean <Thomas.dean.iii1002@gmail.com>
2025-12-01 18:33:31 -0500

    push
    Darrys house set up
    Dialogue in chase set up
    argument dialogue set up just need to include movement

--- Assets/Scripts/Enemy Scripts/SpawnChasingEnemy.cs
+++ Assets/Scripts/Enemy Scripts/SpawnChasingEnemy.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class SpawnChasingEnemy : MonoBehaviour
 {
     public GameObject Enemy;
+    public string hintMessage = "Hurry Echo that thing is still chasing after Darry we need to help him!";
+    public bool hasPassed = false;
 
     private void Start()
     {
@@ -21,6 +23,17 @@ public class SpawnChasingEnemy : MonoBehaviour
         {
             Enemy.SetActive(true);
             Debug.Log("The chasing enemy has spawned.");
+
+            if (hasPassed == false)
+            {
+                ChimeHintUI hintUI = FindObjectOfType<ChimeHintUI>();
+                if (hintUI != null)
+                {
+                    hintUI.ShowHintMessage(hintMessage);
+                }
+            }
+
+            hasPassed = true;
         }
     }
 }
