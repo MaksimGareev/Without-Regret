db65747a9d52c9d0d74774776d90fea4cd67a521
MaksimGareev <maksim322666@gmail.com>
2026-02-23 18:58:13 -0500

    tmcc Work in progress
    Chime appearing VFX, apparitions shader, fog start

---
+++ Assets/VFX/ChimeSpawn/spawn_test.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class spawn_test : MonoBehaviour
+{
+    [Header("Spawn Shader Settings")]
+    public float duration = 2f; // Time to go from 0 to 1
+
+    private Material materialInstance;
+    private float timer = 0f;
+    private bool isAnimating = false;
+
+    void Start()
+    {
+        Renderer renderer = GetComponent<Renderer>();
+        materialInstance = renderer.material;
+
+        // start at 0
+        materialInstance.SetFloat("_NoiseAmnt", 0f);
+    }
+
+    void Update()
+    {
+        // rress 0 key to start animation
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            timer = 0f;
+            isAnimating = true;
+            materialInstance.SetFloat("_NoiseAmnt", 0f);
+        }
+//update shader value over time
+        if (isAnimating)
+        {
+            timer += Time.deltaTime;
+
+            float value = Mathf.Clamp01(timer / duration);
+            materialInstance.SetFloat("_NoiseAmnt", value);
+            //stop animating once we reach 1
+
+            if (value >= 1f)
+            {
+                isAnimating = false;
+            }
+        }
+    }
+}
