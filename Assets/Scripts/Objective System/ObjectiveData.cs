f100bccdf28879441958df17ec1b9c34842dcc44
VictorAA1116 <156972036+VictorAA1116@users.noreply.github.com>
2026-02-23 17:08:25 -0500

    Commented most of the Objective system

--- Assets/Scripts/Objective System/ObjectiveData.cs
+++ Assets/Scripts/Objective System/ObjectiveData.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 
+// This scriptable object holds the data for an objective, which can be used by the Objective Manager to track progress and completion of objectives. 
+// It also contains information for display purposes in the Journal UI and the Objective Popup UI, as well as dialogue for Chime's Hint UI.
 [CreateAssetMenu(fileName = "New Objective", menuName = "Objectives / Objective Data")]
 public class ObjectiveData : ScriptableObject
 {
@@ -10,8 +12,11 @@ public class ObjectiveData : ScriptableObject
     [Tooltip("Title of the objective. This is used for display purposes in the Journal UI and the Objective Popup UI.")]
     public string title; // Title shown in the Journal UI
 
-    [Tooltip("Description of the objective. This is used for display purposes in the Journal UI.")]
-    [TextArea] public string description; // Description shown in the Journal UI
+    [Tooltip("Description of the objective. This is used for display purposes in the Journal UI. It should be written as if the objective is currently active in order to guide the player towards completing the objective.")]
+    [TextArea] public string description; // Description shown in the Journal UI for active objectives
+
+    [Tooltip("Recap of the objective. This is used for display purposes in the Journal UI. It should be written as if the objective has already been completed in order to remind the player of what has already happened, and what choices they have made.")]
+    [TextArea] public string recap; // Recap shown in the Journal UI for comleted objectives
 
     [Tooltip("Dialogue shown by Chime's Hint UI for this objective. This is used to give the player hints or additional information about the objective when they press the ChimeHint Button.")]
     [TextArea] public string chimeDialogue; // Dialogue shown by Chime's Hint UI for this objective
