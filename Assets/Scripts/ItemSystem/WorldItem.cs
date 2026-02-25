c194afdd0bc94dc3ec0c7019e0c01e061e944117
VictorAA1116 <156972036+VictorAA1116@users.noreply.github.com>
2026-02-24 23:15:00 -0500

    Update WorldItem.cs

--- Assets/Scripts/ItemSystem/WorldItem.cs
+++ Assets/Scripts/ItemSystem/WorldItem.cs
@@ -20,23 +20,11 @@ public class WorldItem : MonoBehaviour, IInteractable
 
     [Header("Player Animation")]
     public float animationDuration = 1.5f; // Duration of the collect animation in seconds
-    private Coroutine collectCoroutine;
-    private Animator animator;
-    private Transform player;
-    private PlayerController playerController;
     [HideInInspector] public bool hasBeenCollected = false;
     public ItemData ItemData => itemData;
 
     public void Start()
     {
-        // Player reference
-        player = GameObject.FindGameObjectWithTag("Player")?.transform;
-        playerController = player.GetComponent<PlayerController>();
-        if (player != null)
-        {
-            animator = player.GetComponentInChildren<Animator>();
-        }
-
         if (hasBeenCollected)
         {
             gameObject.SetActive(false);
