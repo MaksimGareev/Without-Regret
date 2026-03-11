f86674d6eb02f01b23c8e65bbf6daedf5af1c687
da413 <danielwolfcol@gmail.com>
2026-03-11 00:02:32 -0400

    updated updated player see through script

--- Assets/VFX/SeeTrough/ForPlayerSeeTroughScript.cs
+++ Assets/VFX/SeeTrough/ForPlayerSeeTroughScript.cs
@@ -5,33 +5,51 @@ public class ForPlayerSeeTroughScript : MonoBehaviour
     public static int PlayerPosID = Shader.PropertyToID("_PlayerPosition");
     public static int SizeID = Shader.PropertyToID("_Size");
 
-     Material WallMaterial;
+    // [SerializeField] Material WallMaterial;
     [SerializeField] Camera Cam;
     [SerializeField] LayerMask Mask;
+
+    [SerializeField] Material[] WallMaterials;
+
+   
 
     private RaycastHit hit; //the raycast hit that returns information about the material
 
     void Update()
     {
+        // Send player position to shader in viewport space
+        var view = Cam.WorldToViewportPoint(transform.position);
+        
+        
+       
+        
         // Direction from player to camera
         var dir = Cam.transform.position - transform.position;
 
         // Ray from player toward camera
-        //var ray = new Ray(transform.position, dir.normalized);
+        var ray = new Ray(transform.position, dir.normalized);
 
         // Check if something blocks the player
-        if (Physics.Raycast(transform.position, dir, out hit, Mask)){
-            WallMaterial =  hit.collider.gameObject.GetComponent<Material>(); //gets the material of whatever the raycast hit and assigns it to this variable
-            WallMaterial.SetFloat(SizeID, 1f);
+        if (Physics.Raycast(ray, out hit, 3000, Mask)){
+           
+           for(int i = 0; i<WallMaterials.Length; ++i) 
+            WallMaterials[i].SetFloat(SizeID, 1f);
+        
         }
         else
         {
-            WallMaterial.SetFloat(SizeID, 0f);
+            for(int i = 0; i<WallMaterials.Length; ++i) 
+                 WallMaterials[i].SetFloat(SizeID, 0f);
         }
-        
 
         // Send player position to shader in viewport space
-        var view = Cam.WorldToViewportPoint(transform.position);
-        WallMaterial.SetVector(PlayerPosID, view);
+        
+        //WallMaterial.SetVector(PlayerPosID, view);
+        
+        for(int i = 0; i<WallMaterials.Length; ++i) 
+            WallMaterials[i].SetVector(PlayerPosID, view);
+        
+           
+        
     }
 }
