dbc4163e58296aa1972842f9ed75a4625e3c0315
da413 <danielwolfcol@gmail.com>
2026-03-10 21:56:06 -0400

    updated ForPlayerSeeTrough
    - i wish i could change the name

--- Assets/VFX/SeeTrough/ForPlayerSeeTroughScript.cs
+++ Assets/VFX/SeeTrough/ForPlayerSeeTroughScript.cs
@@ -5,9 +5,11 @@ public class ForPlayerSeeTroughScript : MonoBehaviour
     public static int PlayerPosID = Shader.PropertyToID("_PlayerPosition");
     public static int SizeID = Shader.PropertyToID("_Size");
 
-    public Material WallMaterial;
-    public Camera Cam;
-    public LayerMask Mask;
+     Material WallMaterial;
+    [SerializeField] Camera Cam;
+    [SerializeField] LayerMask Mask;
+
+    private RaycastHit hit; //the raycast hit that returns information about the material
 
     void Update()
     {
@@ -15,13 +17,18 @@ public class ForPlayerSeeTroughScript : MonoBehaviour
         var dir = Cam.transform.position - transform.position;
 
         // Ray from player toward camera
-        var ray = new Ray(transform.position, dir.normalized);
+        //var ray = new Ray(transform.position, dir.normalized);
 
         // Check if something blocks the player
-        if (Physics.Raycast(ray, 3000, Mask))
+        if (Physics.Raycast(transform.position, dir, out hit, Mask)){
+            WallMaterial =  hit.collider.gameObject.GetComponent<Material>(); //gets the material of whatever the raycast hit and assigns it to this variable
             WallMaterial.SetFloat(SizeID, 1f);
+        }
         else
+        {
             WallMaterial.SetFloat(SizeID, 0f);
+        }
+        
 
         // Send player position to shader in viewport space
         var view = Cam.WorldToViewportPoint(transform.position);
