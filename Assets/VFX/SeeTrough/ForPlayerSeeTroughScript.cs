fa54ca478b92cd2d7fe1671961e4ea1142e083da
VictorAA1116 <156972036+VictorAA1116@users.noreply.github.com>
2026-03-25 00:05:00 -0400

    Updated see through to sphere cast, applied overrides to gamemanager

--- Assets/VFX/SeeTrough/ForPlayerSeeTroughScript.cs
+++ Assets/VFX/SeeTrough/ForPlayerSeeTroughScript.cs
@@ -30,11 +30,14 @@ public class ForPlayerSeeTroughScript : MonoBehaviour
 
             // Direction from player to camera
             var dir = Cam.transform.position - transform.position;
+            
+            // Offset starting point to account for a sphere cast instead of raycast
+            Vector3 rayOrigin = transform.position + (dir.normalized * 2.0f);
 
             // Ray from player toward camera
-            var ray = new Ray(transform.position, dir.normalized);
+            var ray = new Ray(rayOrigin, dir.normalized);
 
-            RayArray = Physics.RaycastAll(ray, 3000, Mask);
+            RayArray = Physics.SphereCastAll(ray,1.1f, 3000, Mask);
 
             if (CurrentObjects != null)
             {
