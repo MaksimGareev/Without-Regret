d4adaa60785a1221c782a67c3ca4a39b93d4512b
VictorAA1116 <156972036+VictorAA1116@users.noreply.github.com>
2026-03-24 20:19:24 -0400

    Update Inventory.cs

--- Assets/Scripts/ItemSystem/Inventory.cs
+++ Assets/Scripts/ItemSystem/Inventory.cs
@@ -180,7 +180,11 @@ public class Inventory : MonoBehaviour, ISaveable
 
             if (!System.Array.Exists(scenesWOPickupEffect, scene => scene == SceneManager.GetActiveScene().name))
             {
-                cameraMovement?.TriggerPickupCameraEffect(itemToCollect.transform);
+                if (cameraMovement)
+                {
+                    cameraMovement.TriggerPickupCameraEffect(itemToCollect.transform);
+                }
+                
                 StartCoroutine(WaitForCameraTransition());
             }
             
