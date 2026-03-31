f0a1eed9d0cf4292a966803b4ff39dbbcdf5ef95
VictorAA1116 <156972036+VictorAA1116@users.noreply.github.com>
2026-03-30 21:18:59 -0400

    Update ObjectiveMarker.cs

--- Assets/Scripts/Objective System/ObjectiveMarker.cs
+++ Assets/Scripts/Objective System/ObjectiveMarker.cs
@@ -32,11 +32,11 @@ public class ObjectiveMarker : MonoBehaviour
         {
             ScreenSpaceIndicator.target = WorldIndicator.transform;
         }
-        ObjectiveManager.Instance.OnObjectiveActivated.AddListener(ObjectiveCompleted);
+        ObjectiveManager.Instance.OnObjectiveActivated.AddListener(ObjectiveActivated);
         ObjectiveManager.Instance.OnObjectiveProgressUpdated.AddListener(ObjectiveProgressed);
     }
 
-    private void ObjectiveCompleted(ObjectiveInstance objective)
+    private void ObjectiveActivated(ObjectiveInstance objective)
     {
         objectiveTransformIndex = 0;
         Refresh(objective, SceneManager.GetActiveScene());
@@ -46,31 +46,41 @@ public class ObjectiveMarker : MonoBehaviour
     {
         if (!objective.data) return;
         
-        if (objective.data.markerTransforms != null)
+        if (objective.data.markerTransforms != null && objective.data.markerTransforms.Count > 0)
         {
             if (int.Equals(scene.buildIndex, objective.data.sceneIndex))
             {
-                if (objective.data.markerTransforms[objectiveTransformIndex] != new Vector3(0, 0, 0))
+                // Ensure the index is within bounds
+                if (objectiveTransformIndex < objective.data.markerTransforms.Count)
                 {
-                    Debug.Log("moving Marker");
-                    gameObject.transform.position = objective.data.markerTransforms[objectiveTransformIndex];
+                    if (objective.data.markerTransforms[objectiveTransformIndex] != new Vector3(0, 0, 0))
+                    {
+                        Debug.Log("moving Marker");
+                        gameObject.transform.position = objective.data.markerTransforms[objectiveTransformIndex];
+                    }
+                    else
+                    {
+                        ScreenSpaceIndicator.disableIndicator = true;
+                        ScreenSpaceIndicator.disableOnScreenIndicator = true;
+                        Debug.Log("No vector value given");
+                        return;
+                    }
+                    
+                    if (objective.data.hasMarker)
+                    {
+                        ScreenSpaceIndicator.disableOnScreenIndicator = false;
+                    }
+                    
+                    if (objective.data.hasOffScreenMarker)
+                    {
+                        ScreenSpaceIndicator.disableIndicator = false;
+                    }
                 }
                 else
                 {
+                    Debug.Log("Marker transform index out of bounds");
                     ScreenSpaceIndicator.disableIndicator = true;
                     ScreenSpaceIndicator.disableOnScreenIndicator = true;
-                    Debug.Log("No vector value given");
-                    return;
-                }
-                
-                if (objective.data.hasMarker)
-                {
-                    ScreenSpaceIndicator.disableOnScreenIndicator = false;
-                }
-                
-                if (objective.data.hasOffScreenMarker)
-                {
-                    ScreenSpaceIndicator.disableIndicator = false;
                 }
             }
             else
@@ -81,13 +91,20 @@ public class ObjectiveMarker : MonoBehaviour
                 return;
             }
         }
-        
+        else
+        {
+            // No marker transforms defined for this objective, disable indicators
+            Debug.Log("No marker transforms defined for objective: " + objective.data.title);
+            ScreenSpaceIndicator.disableIndicator = true;
+            ScreenSpaceIndicator.disableOnScreenIndicator = true;
+        }
     }
 
     private void ObjectiveProgressed(ObjectiveInstance instance)
     {
         objectiveTransformIndex++;
-        if(instance.data.hasMultiplepositions && objectiveTransformIndex<= instance.data.markerTransforms.Count)
+        
+        if(instance.data.hasMultiplepositions && objectiveTransformIndex <= instance.data.markerTransforms.Count)
         {
             gameObject.transform.position = instance.data.markerTransforms[objectiveTransformIndex];
         }
