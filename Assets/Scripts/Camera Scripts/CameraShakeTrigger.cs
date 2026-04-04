5d5f2d1a85003ba46ecc64dcfe3ef9b746905c13
ChaosDais <73835757+ChaosDais@users.noreply.github.com>
2026-04-03 21:39:24 -0400

    add Camera Shake function

---
+++ Assets/Scripts/Camera Scripts/CameraShakeTrigger.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class CameraShakeTrigger : MonoBehaviour
+{
+    [SerializeField] private float shakeDuration = 0.5f;
+    [SerializeField] private float shakeMagnitude = 0.1f;
+    [SerializeField] private float shakeFrequency = 30f;
+    [SerializeField] private bool onlyTriggerOnce = true;
+
+    private CameraMovement cam;
+
+    private void Start()
+    {
+        cam = Camera.main.GetComponent<CameraMovement>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        cam.Shake(shakeDuration, shakeMagnitude, shakeFrequency);
+
+        if (onlyTriggerOnce)
+        {
+            GetComponent<Collider>().enabled = false;
+        }
+    }
+}
