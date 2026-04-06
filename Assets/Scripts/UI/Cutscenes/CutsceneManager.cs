f7d1d5a892ad99cefddbf66bb0914590500a3203
VictorAA1116 <156972036+VictorAA1116@users.noreply.github.com>
2026-04-06 15:15:06 -0400

    Update CutsceneManager.cs

--- Assets/Scripts/UI/Cutscenes/CutsceneManager.cs
+++ Assets/Scripts/UI/Cutscenes/CutsceneManager.cs
@@ -4,20 +4,39 @@ using UnityEngine.UI;
 public class CutsceneManager : MonoBehaviour
 {
     [Header("References")]
+    [SerializeField] private GameObject cutscenePanel; 
     [SerializeField] private Image backgroundImage;
     private CutsceneData currentCutscene;
+    
+    [HideInInspector] public bool isCutscenePlaying = false;
 
     public void StartCutscene(CutsceneData cutscene)
     {
+        if (!cutscene)
+        {
+            Debug.LogWarning("No cutscene selected");
+            return;
+        }
+
+        if (isCutscenePlaying)
+        {
+            Debug.LogWarning("Cutscene is already playing, ignoring second call to start cutscene");
+            return;
+        }
         
+        currentCutscene = cutscene;
+
+        if (!cutscenePanel.activeSelf)
+        {
+            cutscenePanel.SetActive(true);
+        }
+        
+        PlayClip(currentCutscene.clips[0]);
+        
+        isCutscenePlaying = true;
     }
 
-    private void EndCutscene()
-    {
-        
-    }
-
-    private void SkipToNextClip()
+    private void SkipCurrentClip()
     {
         
     }
@@ -25,5 +44,13 @@ public class CutsceneManager : MonoBehaviour
     private void PlayClip(CutsceneClip clip)
     {
         backgroundImage.sprite = clip.backgroundImage;
+    }
+    
+    private void EndCutscene()
+    {
+        cutscenePanel.SetActive(false);
+        
+        currentCutscene = null;
+        backgroundImage.sprite = null;
     }
 }
