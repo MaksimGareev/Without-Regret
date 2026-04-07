57f3388a93e4b5f8ac862afc2f4629ef5067c6c2
VictorAA1116 <156972036+VictorAA1116@users.noreply.github.com>
2026-04-06 20:42:33 -0400

    A Good amount of progress on the Cutscene system

--- Assets/Scripts/UI/Cutscenes/CutsceneManager.cs
+++ Assets/Scripts/UI/Cutscenes/CutsceneManager.cs
@@ -1,14 +1,115 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 public class CutsceneManager : MonoBehaviour
 {
+    public static CutsceneManager Instance { get; private set; }
+    
     [Header("References")]
-    [SerializeField] private GameObject cutscenePanel; 
+    [SerializeField] private GameObject cutscenePanel;
+    [SerializeField] private Button continueButton;
     [SerializeField] private Image backgroundImage;
+    [SerializeField] private Image dialogueBackgroundImage;
+    [SerializeField] private Slider holdToSkipSlider;
+    
+    [Header("Art Assets")]
+    [SerializeField] private Sprite continueButtonSprite;
+    [SerializeField] private Sprite dialogueBackgroundSprite;
+    
+    [Header("Input Action Assets")]
+    [SerializeField] private InputActionAsset inputActions;
+    private InputAction confirmAction;
+
+    [Header("Settings")] 
+    [Tooltip("The time in seconds that the player has to hold the confirm input in order to skip the entire cutscene.")]
+    [SerializeField, Range(0.5f, 5.0f)] private float holdToSkipDuration = 1.0f;
+    private float skipTimer = 0.0f;
+    private bool isHoldingSkip = false;
+
+    private bool canSkipClip = false;
+    private bool canSkipEntireCutscene = false;
+    
     private CutsceneData currentCutscene;
+    private int currentClipIndex;
+    private Coroutine currentClipCoroutine;
     
     [HideInInspector] public bool isCutscenePlaying = false;
+
+    private void Awake()
+    {
+        if (Instance && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    private void OnEnable()
+    {
+        InitializeInputActions();
+    }
+
+    private void Update()
+    {
+        ProcessHoldToSkip();
+    }
+    
+    private void InitializeInputActions()
+    {
+        // Initialize input actions
+        confirmAction = inputActions.FindActionMap("UI").FindAction("Confirm");
+        if (confirmAction== null)
+        {
+            Debug.LogError("Confirm action not found in InputActionAsset.");
+            return;
+        }
+
+        confirmAction.started += OnConfirmStarted;
+        confirmAction.canceled += OnConfirmCancelled;
+        confirmAction.Enable();
+    }
+
+    private void ProcessHoldToSkip()
+    {
+        if (!isHoldingSkip) return;
+        
+        skipTimer += Time.deltaTime;
+        
+        if (skipTimer >= holdToSkipDuration && canSkipEntireCutscene)
+        {
+            EndCutscene();
+            ResetHoldTimer();
+        }
+    }
+
+    private void OnConfirmStarted(InputAction.CallbackContext ctx)
+    {
+        isHoldingSkip = true;
+        skipTimer = 0.0f;
+    }
+
+    private void OnConfirmCancelled(InputAction.CallbackContext ctx)
+    {
+        ResetHoldTimer();
+    }
+
+    private void ResetHoldTimer()
+    {
+        if (skipTimer < holdToSkipDuration && skipTimer > 0.02f && canSkipClip)
+        {
+            SkipCurrentClip();
+        }
+        
+        isHoldingSkip = false;
+        skipTimer = 0.0f;
+    }
 
     public void StartCutscene(CutsceneData cutscene)
     {
@@ -31,19 +132,84 @@ public class CutsceneManager : MonoBehaviour
             cutscenePanel.SetActive(true);
         }
         
-        PlayClip(currentCutscene.clips[0]);
+        PlayClip(currentCutscene.clips[currentClipIndex]);
         
         isCutscenePlaying = true;
+
+        if (currentCutscene.canSkipEntireCutscene)
+        {
+            StartCoroutine(WaitToSkipEntireCutscene());
+        }
+    }
+
+    private IEnumerator WaitToSkipEntireCutscene()
+    {
+        yield return new WaitForSecondsRealtime(0.5f);
+        
+        canSkipEntireCutscene = true;
     }
 
     private void SkipCurrentClip()
     {
+        if (currentClipCoroutine != null)
+        {
+            StopCoroutine(currentClipCoroutine);
+        }
         
+        HideContinueButton();
+        
+        currentClipIndex++;
+        
+        if (currentCutscene.clips.Length <= currentClipIndex)
+        {
+            EndCutscene();
+            return;
+        }
+        
+        PlayClip(currentCutscene.clips[currentClipIndex]);
+    }
+
+    private void ShowContinueButton()
+    {
+        continueButton.gameObject.SetActive(true);
+    }
+
+    private void HideContinueButton()
+    {
+        continueButton.gameObject.SetActive(false);
     }
 
     private void PlayClip(CutsceneClip clip)
     {
         backgroundImage.sprite = clip.backgroundImage;
+        
+        // Play appropriate Dialogue
+        
+        currentClipCoroutine = StartCoroutine(WaitForClipDuration());
+    }
+
+    private IEnumerator WaitForClipDuration()
+    {
+        // Early skip allowing if canSkip overrides duration
+        yield return new WaitForSecondsRealtime(0.5f);
+
+        if (currentCutscene.clips[currentClipIndex].canSkipClipEarly)
+        {
+            canSkipClip = true;
+            ShowContinueButton();
+        }
+        
+        yield return new WaitForSecondsRealtime(currentCutscene.clips[currentClipIndex].duration - 0.5f);
+
+        if (currentCutscene.clips[currentClipIndex].autoContinue)
+        {
+            SkipCurrentClip();
+        }
+        else
+        {
+            canSkipClip = true;
+            ShowContinueButton();
+        }
     }
     
     private void EndCutscene()
@@ -52,5 +218,7 @@ public class CutsceneManager : MonoBehaviour
         
         currentCutscene = null;
         backgroundImage.sprite = null;
+        
+        isCutscenePlaying = false;
     }
 }
---
+++ Assets/Scripts/UI/Cutscenes/CutsceneTriggerVolume.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class CutsceneTriggerVolume : MonoBehaviour
+{
+    [Tooltip("The cutscene Data to be played when the player enters this trigger volume")]
+    [SerializeField] private CutsceneData cutsceneToPlay;
+
+    [Tooltip("The Objective that should be ACTIVE to be able to play this cutscene")] 
+    [SerializeField] private ObjectiveData linkedObjective;
+    
+    [Tooltip("If true, the player will need to have the linked objective active in order for the cutscene to play. If false, the cutscene will play regardless of the state of the linked objective.")]
+    [SerializeField] private bool needsObjective;
+    
+    private bool playedOnce;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        bool shouldPlay =
+            (
+                other.CompareTag("Player")
+                && cutsceneToPlay
+                && !playedOnce
+                && (linkedObjective && needsObjective)
+                && CutsceneManager.Instance
+            );
+
+        if (!shouldPlay) return;
+        
+        CutsceneManager.Instance.StartCutscene(cutsceneToPlay);
+        playedOnce = true;
+    }
+}
