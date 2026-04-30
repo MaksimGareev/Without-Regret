0fed771a7fbc8a5f4b2330ac3199bcfa029e8cce
Tom-Dean <Thomas.dean.iii1002@gmail.com>
2026-04-29 21:06:27 -0400

    Moonray despawn

--- Assets/Scripts/SpawnMoonRay.cs
+++ Assets/Scripts/SpawnMoonRay.cs
@@ -3,11 +3,20 @@ using UnityEngine;
 public class SpawnMoonRay : MonoBehaviour
 {
     public GameObject MoonRay;
+    public NewDialogueTrigger DialogueTrigger;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             MoonRay.SetActive(true);
+        }
+    }
+
+    private void Update()
+    {
+        if (DialogueTrigger && DialogueTrigger.completed)
+        {
+            MoonRay.SetActive(false);
         }
     }
 }
